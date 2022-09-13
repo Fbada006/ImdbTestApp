21e11b459b2a46dca15d3dbfb267d73040087adc
Fbada006 <ferdinandkamata@yahoo.com>
2022-09-13 20:49:56 +0300

    add correct entities to database and use room paging artifact

--- app/src/main/java/com/example/imdbtestapp/data/ImdbMovieDatabase.kt
+++ app/src/main/java/com/example/imdbtestapp/data/ImdbMovieDatabase.kt
@@ -2,9 +2,11 @@ package com.example.imdbtestapp.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.imdbtestapp.data.models.DbMovie
+import com.example.imdbtestapp.data.models.DbRemoteKeys
 
 @Database(
-    entities = [MovieDao::class],
+    entities = [DbMovie::class, DbRemoteKeys::class],
     version = 1,
     exportSchema = false
 )
