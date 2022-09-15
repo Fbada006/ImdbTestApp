2d0ace71d625d56b2532feeabbb5de78b7a3162f
Fbada006 <ferdinandkamata@yahoo.com>
2022-09-15 12:17:38 +0300

    add back navigation where necesary

--- app/src/main/java/com/example/tmdbtestapp/navigation/TmdbDestinations.kt
+++ app/src/main/java/com/example/tmdbtestapp/navigation/TmdbDestinations.kt
@@ -1,5 +1,8 @@
 package com.example.tmdbtestapp.navigation
 
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+
 interface TmdbDestinations {
     val route: String
 }
@@ -10,4 +13,14 @@ object MainScreen : TmdbDestinations {
 
 object DetailScreen : TmdbDestinations {
     override val route = "com.example.tmdbtestapp.detailscreen"
+    const val movieJsonArg = "movieJson"
+    val routeWithArgs = "${route}/{${movieJsonArg}}"
+    val arguments = listOf(
+        navArgument(movieJsonArg) { type = NavType.LongType }
+    )
 }
+
+// These are destinations that should have a back button
+val popDestinations = listOf(
+    DetailScreen.route
+)
--- app/src/main/java/com/example/tmdbtestapp/navigation/TmdbNavHost.kt
+++ app/src/main/java/com/example/tmdbtestapp/navigation/TmdbNavHost.kt
@@ -1,28 +1,46 @@
 package com.example.tmdbtestapp.navigation
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.tmdbtestapp.ui.TmdbViewmodel
 import com.example.tmdbtestapp.ui.details.MovieDetails
 import com.example.tmdbtestapp.ui.main.MainScreen
 
 @Composable
 fun TmdbNavHost(
     navController: NavHostController,
-    paddingValues: PaddingValues
+    paddingValues: PaddingValues,
+    tmdbViewmodel: TmdbViewmodel
 ) {
     NavHost(
         navController = navController,
         startDestination = MainScreen.route
     ) {
         composable(route = MainScreen.route) {
-            MainScreen(paddingValues = paddingValues)
+            MainScreen(paddingValues = paddingValues, tmdbViewmodel = tmdbViewmodel, onMovieClick = { movie ->
+                navController.navigateToDetailScreen(movie.id)
+            })
         }
-        composable(route = DetailScreen.route) {
-            MovieDetails()
+        composable(
+            route = DetailScreen.routeWithArgs,
+            arguments = DetailScreen.arguments
+        ) { navBackStackEntry ->
+            val movieId =
+                navBackStackEntry.arguments?.getLong(DetailScreen.movieJsonArg)
+            MovieDetails(
+                movieId = movieId, tmdbViewmodel = tmdbViewmodel, modifier = Modifier
+                    .wrapContentHeight()
+                    .padding(all = 4.dp)
+            )
         }
     }
 }
@@ -37,3 +55,7 @@ private fun NavHostController.navigateSingleTopTo(route: String) =
         launchSingleTop = true
         restoreState = true
     }
+
+fun NavHostController.navigateToDetailScreen(movieId: Long) {
+    this.navigateSingleTopTo("${DetailScreen.route}/$movieId")
+}
